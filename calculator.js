9eabc41c766b9dab56ea8988c8d75c25d494a4d1
KEVIN CHAU <kevinch@udel.edu>
2023-10-19 20:23:53 -0400

    finished basis of calculator functions

---
+++ calculator.js
@@ -0,0 +1,20 @@
+let firstNum;
+let operator;
+let secondNum;
+
+
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b) {
+    return a-b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide (a,b){
+    return a/b;
+}
+
+function operate(firstNum, operator, secondNum){
+}
