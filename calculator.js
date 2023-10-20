5b435be302675c6cf9bae277eeb63df6754a5764
KEVIN CHAU <kevinch@udel.edu>
2023-10-20 12:17:54 -0400

    Reformatted buttons and changed their classes. Allowed buttons to update display. Clear and delete buttons work

--- calculator.js
+++ calculator.js
@@ -1,20 +1,54 @@
 let firstNum;
 let operator;
 let secondNum;
+let zeroNumber = true;
+let selectedOperator = false;
 
-
-function add(a,b){
-    return a + b;
+function clear(){
+    firstNum = '';
+    operator = '';
+    selectedOperator = false;
+    zeroNumber = true;
+    currentText.textContent = "0";
 }
-function subtract(a,b) {
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
     return a-b;
 }
-function multiply(a,b){
-    return a * b;
+function divide(a,b){
+    return a/b
 }
-function divide (a,b){
-    return a/b;
+function multiply(a,b){
+    return a*b;
+}
+function operate(first, operator, second){
+}
+let deleteBtn = document.querySelector('[data-delete]');
+deleteBtn.addEventListener('click', function(){
+    let text = currentText.textContent;
+    if (currentText.textContent !== "0"){
+        text = text.slice(0,-1)
+    }
+    currentText.textContent = text;
+})
+
+
+let currentText = document.querySelector('#result');
+if (zeroNumber === true){
+    currentText.textContent = "0";
 }
 
-function operate(firstNum, operator, secondNum){
-}
+//adding event Listener to each button
+let numberButtons = document.querySelectorAll("[data-number]")
+numberButtons.forEach(button => button.addEventListener('click',() => {
+    if (zeroNumber){
+        zeroNumber = false;
+        currentText.textContent = "";
+    }
+    currentText.textContent += button.textContent }))
+
+
+let clearButton = document.querySelector('[data-all-clear]')
+clearButton.addEventListener('click', () => clear())
